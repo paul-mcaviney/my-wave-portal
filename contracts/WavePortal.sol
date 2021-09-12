e1e81efd0923b5bd2d0bef2669d3a9f8de3c65b2
Paul McAviney <87715498+paul-mcaviney@users.noreply.github.com>
2021-09-12 15:32:51 -0600

    Initial commit

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: Unlicensed
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor() {
+        console.log("This is one helluva smart contract!");
+    } 
+}
+
+// test
