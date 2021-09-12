a7d0d5f341e2b64155112d852a00874cd058573b
Paul McAviney <87715498+paul-mcaviney@users.noreply.github.com>
2021-09-12 16:22:17 -0600

    Get Smart Contract Working

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,9 +5,19 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint totalWaves;
     constructor() {
         console.log("This is one helluva smart contract!");
     } 
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() view public returns (uint) {
+        console.log("We have received %d total waves", totalWaves);
+        return totalWaves;
+    }
 }
 
-// test
